dbdf095d24379b68cc472c76d7d9885d62026398
ejdrp1 <ejdrp1@naver.com>
2023-05-31 21:59:50 +0900

    05.31 메인페이지

--- src/component/js/ConnectHeader.js
+++ src/component/js/ConnectHeader.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import '../scss/ConnectHeader.scss';
+import ConnectHotPlace from './ConnectHotPlace';
 
 const ConnectHeader = () => {
   return (
@@ -19,7 +20,7 @@ const ConnectHeader = () => {
             <ul className='ch-menu'>
                 {/* header 메뉴 list */}
                 <li className='ch-menu-list'><a href='#' className='list'>홈</a></li>
-                <li className='ch-menu-list'><a href='#' className='list'>Hot Place</a></li>
+                <li className='ch-menu-list'><a href={<ConnectHotPlace />} className='list'>Hot Place</a></li>
                 <li className='ch-menu-list'><a href='#' className='list'>동네 실시간 채팅</a></li>
                 <li className='ch-menu-list'><a href='#' className='list'>동네 친구 만들기</a></li>
                 <li className='ch-menu-list'><a href='#' className='list'>동네 마감 세일</a></li>
@@ -27,16 +28,18 @@ const ConnectHeader = () => {
             </ul>
         </div>
     </div>
-    
+
     {/* 로그인 버튼 box */}
     <div className='connect-header-login-box'>
         {/* 로그인 버튼 */}
         <a href='#' id='Login'>로그인</a>
-    </div>
-    {/* 회원가입 버튼 box */}
-    <div className='connect-header-signin-box'>
-        {/* 회원가입 버튼 */}
-        <a href='#' id='Sign-in'>회원가입</a>
+        <span>
+            {/* 회원가입 버튼 box */}
+            <div className='connect-header-signin-box'>
+                {/* 회원가입 버튼 */}
+                <a href='#' id='Sign-in'>회원가입</a>
+            </div>
+        </span>
     </div>
     </>
   )
---
+++ src/component/js/ConnectHotPlace.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ConnectHotPlace = () => {
+  return (
+    <>
+    <div>ConnectHotPlace</div>
+    <div>ConnectHotPlace</div>
+    <div>ConnectHotPlace</div>
+    <div>ConnectHotPlace</div>
+    </>
+  )
+}
+
+export default ConnectHotPlace
