f721895bf84b377554d0f0207e0709fae0961ab6
ejdrp1 <ejdrp1@naver.com>
2023-06-15 20:49:04 +0900

    06.15 임시 저장

--- src/component/js/ConnectMainOutline.js
+++ src/component/js/ConnectMainOutline.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom';
 import { Link, Route, Routes } from 'react-router-dom';
 import ConnectHotPlace from './ConnectHotPlace';
@@ -6,11 +6,38 @@ import ConnectFreeBoard from './ConnectFreeBoard';
 import ConnectPlayList from './ConnectPlayList';
 import ConnectStoreInfo from './ConnectStoreInfo';
 import ConnectSales from './ConnectSales';
-
-import '../scss/ConnectMainOutline.scss';
 import ConnectLiveChatting from './ConnectLiveChatting';
 
+import '../scss/ConnectMainOutline.scss';
+
 const ConnectMainOutline = () => {
+
+    const [isChangeOutline, setIsChangeOutline] = useState(true);
+    const [isChangeStore, setIsChangeStore] = useState(true);
+
+    const closeChangeMenu = e => {
+        const $menuClose = document.getElementById('CmoMenu');
+        
+        if($menuClose) {
+            $menuClose.style.animation = 'closeOutline 2s forwards 1';
+        }
+
+        setTimeout(() => {
+            setIsChangeOutline(false);
+            setIsChangeStore(false);
+        }, 1500);
+    };
+    
+    const openChangeMenu = e => {
+        setIsChangeOutline(true);
+    };
+
+
+
+
+
+
+
 
     const $location = useLocation();
 
@@ -18,51 +45,75 @@ const ConnectMainOutline = () => {
         return null;
     }
 
-    const clickList = e => {
-        const $list = document.querySelector('.cmo-list');
-        
-    };
+
 
    
   return (
     <>
-        {/* 고정 메인 전체 */}
+
         <div className='connect-main-outline-wrapper'>
-            {/* 고정 메인 header */}
             <header className='cmo-h1-box'>
-                {/* 고정 메인 header text */}
                 <h1>우리동네 100% 즐기기!</h1>
             </header>
 
-            {/* 고정 메인 container */}
             <div className='cmo-container'>
-                {/* 고정 메인 메뉴 box */}
-                <ul className='cmo-menu'>
-                    <Link to={'/contents/hot-place'} className='cmo-list' id='Hot-Place' onClick={ clickList }>
-                        <div className='link-box'>
-                            <li className='cmo-menu-list hot-place'></li>
-                            <p className='cmo-text'>Hot Place</p>
-                        </div>
+
+                {!isChangeStore && (
+                    <div
+                        className={`store-menu-modal-wrapper ${
+                            isChangeOutline ? 'openAnimation' : 'closeAnimation'
+                        }`}
+                        id='StoreModal'
+                    ><ConnectSales />
+                </div>
+                )}
+
+
+
+
+
+
+
+
+                    <ul className='cmo-menu' id='CmoMenu'>
+                    <Link to={'/contents/hot-place'} className='cmo-list' id='Hot-Place'>
+                        {isChangeOutline && (
+                            <div className='link-box'>
+                                <li className='cmo-menu-list hot-place'></li>
+                                <p className='cmo-text'>Hot Place</p>
+                            </div>
+                        )}
                     </Link>
-                    <Link to={'/contents/free-board'} className='cmo-list' id='Real-time-Chatting' onClick={ clickList }>
-                        <div className='link-box'>
-                            <li className='cmo-menu-list real-time-chatting'></li>
-                            <p className='cmo-text'>동네 자유 게시판</p>
-                        </div>
+                    <Link to={'/contents/free-board'} className='cmo-list' id='Real-time-Chatting'>
+                        {isChangeOutline && (
+                            <div className='link-box'>
+                                <li className='cmo-menu-list real-time-chatting'></li>
+                                <p className='cmo-text'>동네 자유 게시판</p>
+                            </div>
+                        )}
                     </Link>
-                    <Link to={'/nb-playlist'} className='cmo-list' id='Making-Friends' onClick={ clickList }>
-                        <div className='link-box'>
-                            <li className='cmo-menu-list making-friends'></li>
-                            <p className='cmo-text'>동네 플레이 리스트</p>
-                        </div>
+                    <Link to={'/nb-playlist'} className='cmo-list' id='Making-Friends'>
+                        {isChangeOutline && (
+                            <div className='link-box'>
+                                <li className='cmo-menu-list making-friends'></li>
+                                <p className='cmo-text'>동네 플레이 리스트</p>
+                            </div>
+                        )}
                     </Link>
-                    <Link to={'/nb-closing-sale'} className='cmo-list' id='Closing-sale' onClick={ clickList }>
-                        <div className='link-box'>
-                            <li className='cmo-menu-list closing-sale'></li>
-                            <p className='cmo-text'>동네 편의점 정보</p>
-                        </div>
+
+                    <Link to={'/contents/csv'} className='cmo-list' id='Closing-sale'>
+                        <button onClick={ closeChangeMenu } className='cmo-change-btn'>
+                            {isChangeOutline && (
+                                <div className='link-box'>
+                                    <li className='cmo-menu-list closing-sale'></li>
+                                    <p className='cmo-text'>동네 편의점 정보</p>
+                                </div>
+                            )}
+                        </button>
                     </Link>
                 </ul>
+
+
 
                 {/* 고정 메인 메뉴 전환 게시판 box */}
                 <div className='cmo-change-board-box'>
@@ -80,14 +131,13 @@ const ConnectMainOutline = () => {
                             <Route path='/nb-playlist' element={ <ConnectPlayList /> }></Route>
                         </Routes>
                         <Routes>
-                            <Route path='/nb-closing-sale' element={ <ConnectSales /> }></Route>
+                            <Route path='/contents/csv' element={ <ConnectStoreInfo /> }></Route>
                         </Routes>
                         <Routes>
-                            <Route path='/nb-store-event-info' element={ <ConnectStoreInfo /> }></Route>
+                            <Route path='/nb-closing-sale' element={ <ConnectSales /> }></Route>
                         </Routes>
                     </div>
                 </div>
-
 
             </div>
         </div>
--- src/component/js/ConnectStoreInfo.js
+++ src/component/js/ConnectStoreInfo.js
@@ -1,8 +1,220 @@
 import React from 'react'
 
+import '../scss/ConnectStoreInfo.scss';
+
 const ConnectStoreInfo = () => {
+
+
+
+
   return (
-    <div>ConnectStoreInfo</div>
+    <>
+      <div className='store-info-wrapper'>
+        <div className='store-info-box'>
+
+          <header className='store-info-header'>
+            <div className='info-view-all'>
+              <p>전체보기</p>
+            </div>
+            <div className='one-plus-one'>
+              <p>1 + 1</p>
+            </div>
+            <div className='two-plus-one'>
+              <p>2 + 1</p>
+            </div>
+          </header>
+
+
+
+          <div className='store-info-filter-wrapper'>
+            <div className='store-info-filter-box'>
+
+              <div className='store-info-filter'>
+
+                <div className='price-btn-box'>
+                  <button className='price-btn'>
+                    <p>가격순</p>
+                  </button>
+                  <button className='price-btn'>
+                    <p>임시</p>
+                  </button>
+                </div>
+
+                <div className='search-box'>
+                  <div className='input-btn-box'>
+                    <input type='text' className='store-info-input' placeholder='검색어를 입력하세요'/>
+                    <div className='search-btn-box'>
+                      <button className='search-btn'></button>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+
+              <div className='store-info-main-wrapper'>
+                <div className='store-info-main-box'>
+
+                  <div className='store-info-list-box'>
+
+                    <div className='store-info-list'>
+                      <div className='list-header'>
+                        <div className='info-img-box'>
+                          <div className='info-img'></div>
+                        </div>
+                      </div>
+
+                      <div className='list-main'>
+                        <div className='info-name-box'>
+                          <p>초코파이</p>
+                        </div>
+                      </div>
+
+                      <div className='list-footer'>
+                        <div className='info-price-box'>
+                          <p>삼천만원</p>
+                        </div>
+                      </div>
+                    </div>
+
+
+                    <div className='store-info-list'>
+                      <div className='list-header'>
+                        <div className='info-img-box'>
+                          <div className='info-img'></div>
+                        </div>
+                      </div>
+
+                      <div className='list-main'>
+                        <div className='info-name-box'>
+                          <p>초코파이</p>
+                        </div>
+                      </div>
+
+                      <div className='list-footer'>
+                        <div className='info-price-box'>
+                          <p>삼천만원</p>
+                        </div>
+                      </div>
+                    </div>
+
+
+                    <div className='store-info-list'>
+                      <div className='list-header'>
+                        <div className='info-img-box'>
+                          <div className='info-img'></div>
+                        </div>
+                      </div>
+
+                      <div className='list-main'>
+                        <div className='info-name-box'>
+                          <p>초코파이</p>
+                        </div>
+                      </div>
+
+                      <div className='list-footer'>
+                        <div className='info-price-box'>
+                          <p>삼천만원</p>
+                        </div>
+                      </div>
+                    </div>
+
+
+                    <div className='store-info-list'>
+                      <div className='list-header'>
+                        <div className='info-img-box'>
+                          <div className='info-img'></div>
+                        </div>
+                      </div>
+
+                      <div className='list-main'>
+                        <div className='info-name-box'>
+                          <p>초코파이</p>
+                        </div>
+                      </div>
+
+                      <div className='list-footer'>
+                        <div className='info-price-box'>
+                          <p>삼천만원</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    
+
+
+
+
+
+
+                  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                </div>
+              </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+          </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+      </div>
+
+
+
+
+
+
+    </>
   )
 }
 
