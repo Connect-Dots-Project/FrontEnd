b09ea3be5de95f5bed6d0e40e43398ea6e28ecf0
sumin <330sum@naver.com>
2023-06-16 15:34:15 +0900

    [feature] hotplace 1. 전체보기 게시판 렌더링 완료 2. 전체보기 지도형태 렌더링 완료 3. 글쓰기 작업 중

--- src/component/js/ConnectCreatePost.js
+++ src/component/js/ConnectCreatePost.js
@@ -1,90 +1,123 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 import '../scss/ConnectCreatePost.scss';
 import ConnectWriteBoard from './ConnectWriteBoard';
+import Location from './Location';
 
 const ConnectCreatePost = ({ closeCreatePost, closeWriteBoard }) => {
+  const [isCreateModal, setCreateModal] = useState(true);
+  const [selectedDistrict, setSelectedDistrict] = useState('');
+  const [showAllDistricts, setShowAllDistricts] = useState(true);
 
-    const [isCreateModal, setCreateModal] = useState(true);
+  useEffect(() => {
+    if (isCreateModal) {
+      const $modal = document.getElementById('CreatePostModal');
+      $modal.classList.add('opening');
+    }
+  }, [isCreateModal]);
 
-    useEffect(() => {
-        if(isCreateModal) {
-            const $modal = document.getElementById('CreatePostModal');
-            $modal.classList.add('opening');
-        }
-    }, [isCreateModal]);
+  const closeModal = () => {
+    const $modal = document.getElementById('CreatePostModal');
+    $modal.classList.add('closing');
 
-    const closeModal = () => {
-        const $modal = document.getElementById('CreatePostModal');
-        $modal.classList.add('closing');
+    setTimeout(() => {
+      setCreateModal(false);
+      closeCreatePost();
+    }, 1000);
+  };
 
-        setTimeout(() => {
-            setCreateModal(false);
-            closeCreatePost();
-        }, 1000);
-      };
-      
-      const cancelBtn = e => {
-        const $modal = document.getElementById('CreatePostModal');
-        $modal.classList.add('closing');
+  const cancelBtn = () => {
+    const $modal = document.getElementById('CreatePostModal');
+    $modal.classList.add('closing');
 
-        setTimeout(() => {
-            setCreateModal(false);
-            closeCreatePost();
-        }, 1000);
-      };
+    setTimeout(() => {
+      setCreateModal(false);
+      closeCreatePost();
+    }, 1000);
+  };
+
+  const districts = [
+    '강남구', '강동구', '강북구', '강서구', '관악구', '광진구', '구로구', '금천구',
+    '노원구', '도봉구', '동대문구', '동작구', '마포구', '서대문구', '서초구', '성동구',
+    '성북구', '송파구', '양천구', '영등포구', '용산구', '은평구', '종로구', '중구', '중랑구'
+  ];
+
+  const handleSubmit = () => {
+    // ...
+  };
+
+  const handleDistrictClick = (district) => {
+    setSelectedDistrict(district);
+    setShowAllDistricts(false);
+  };
+
+  const handleShowAllDistricts = () => {
+    setSelectedDistrict('');
+    setShowAllDistricts(true);
+  };
+
+  const districtList = showAllDistricts ? (
+    districts.map((district, index) => (
+      <li key={index} className='cp-header-tag' onClick={() => handleDistrictClick(district)}>
+        <p>{district}</p>
+      </li>
+    ))
+  ) : (
+    <li className='cp-header-tag' onClick={handleShowAllDistricts}>
+      <p>{selectedDistrict}</p>
+    </li>
+  );
 
   return (
     <>
-    {isCreateModal && (
-
-    <div className='create-post-wrapper' id='CreatePostModal'>
-        <button className='cp-close-btn' onClick={ closeModal }>X</button>
-            <div className='header-main-footer-box'>
-                
-                <header className='cp-header'>
-                    <div className='cp-header-text-tag-box'>
-                        <div className='cp-header-text-box'>
-                            <p className='cp-header-text'>지역을 선택해주세요</p>
-                        </div>
-
-                        <ul className='cp-header-tag-box'>
-                            <li className='cp-header-tag'><p>강남구</p></li>
-                        </ul>
-                        
-                    </div>
-                </header>
-
-                <div className='cp-main-box'>
-                  <div className='cp-main'>
-                    <ConnectWriteBoard />
-                  </div>
+      {isCreateModal && (
+        <div className='create-post-wrapper' id='CreatePostModal'>
+          <button className='cp-close-btn' onClick={closeModal}>
+            X
+          </button>
+          <div className='header-main-footer-box'>
+            <header className='cp-header'>
+              <div className='cp-header-text-tag-box'>
+                <div className='cp-header-text-box'>
+                  <p className='cp-header-text'>지역을 선택해주세요</p>
                 </div>
 
-                <footer className='cp-footer'>
-                    <div className='cp-footer-text-api-box'>
-                        <div className='cp-footer-text-box'>
-                            <p>장소를 선택해주세요</p>
-                        </div>
-                        <div className='cp-footer-api-box'>
-                          <input className='cp-footer-api' />
-                          <div className='storage-btn-box'>
-                            <button className='api-btn' id='Cancel' onClick={ cancelBtn }>
-                              <p>취 소</p>
-                            </button>
-                            <button className='api-btn' id='Storage'>
-                              <p>저 장</p>
-                            </button>
-                          </div>
-                        </div>
-                    </div>
-                </footer>
+                <ul className='cp-header-tag-box'>
+                  {districtList}
+                </ul>
+              </div>
+            </header>
 
+            <div className='cp-main-box'>
+              <div className='cp-main'>
+                <ConnectWriteBoard />
+              </div>
             </div>
-        </div>
-    )}
-    </>
-  )
-}
 
-export default ConnectCreatePost
+            <footer className='cp-footer'>
+              <div className='cp-footer-text-api-box'>
+                <div className='cp-footer-text-box'>
+                  <p>장소를 선택해주세요</p>
+                </div>
+                <div className='cp-footer-api-box'>
+                  <button className='api-btn' id='Cancel' onClick={cancelBtn}>
+                    <p>취 소</p>
+                  </button>
+                  <button className='api-btn' id='Storage' onClick={handleSubmit}>
+                    <p>저 장</p>
+                  </button>
+                  <div className='cp-footer-api'>
+                    <Location />
+                  </div>
+                  <div className='storage-btn-box'></div>
+                </div>
+              </div>
+            </footer>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ConnectCreatePost;
--- src/component/js/ConnectTotalMap.js
+++ src/component/js/ConnectTotalMap.js
@@ -1,24 +1,82 @@
-import React from 'react'
-
-import '../scss/ConnectTotalMap.scss';
+import { useEffect, useState } from 'react';
+import { Map, MapMarker, MapInfoWindow } from 'react-kakao-maps-sdk';
 
 const ConnectTotalMap = () => {
+  const [hpData, setHpData] = useState([]);
+  const [hoveredLocation, setHoveredLocation] = useState(null);
+  const [selectedLocation, setSelectedLocation] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8181/contents/hot-place', {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        const list = [...result.hotplaceList];
+        console.log(list);
+        setHpData(list);
+      });
+  }, []);
+
+  const handleMarkerClick = (location) => {
+    setSelectedLocation(location);
+  };
+
+  const handleMarkerMouseEnter = (location) => {
+    setHoveredLocation(location);
+  };
+
+  const handleMarkerMouseLeave = () => {
+    setHoveredLocation(null);
+  };
+
+  const locations = hpData.map((item) => {
+    const { hotplaceName, hotplaceLatitude, hotplaceLongitude, hotplaceAddress, hotplaceImage } = item;
+    return {
+      title: hotplaceName,
+      latlng: { lat: parseFloat(hotplaceLatitude), lng: parseFloat(hotplaceLongitude) },
+      address: hotplaceAddress,
+      image: hotplaceImage,
+    };
+  });
+
   return (
-    <>
+    <Map center={{ lat: 37.4996237314472, lng: 127.03051594993698 }} style={{ width: '800px', height: '600px' }} level={3}>
+      {locations.map((loc) => (
+        <MapMarker
+          key={`${loc.title}-${loc.latlng}`}
+          position={loc.latlng}
+          title={loc.title}
+          clickable={true}
+          onClick={() => handleMarkerClick(loc)}
+          onMouseEnter={() => handleMarkerMouseEnter(loc)}
+          onMouseLeave={handleMarkerMouseLeave}
+          image={{
+            src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
+            size: { width: 24, height: 35 },
+          }}
+        />
+      ))}
+      {selectedLocation && (
+        <MapInfoWindow position={selectedLocation.latlng} onClose={() => setSelectedLocation(null)}>
+          <div style={{ backgroundColor: 'white', padding: '10px', borderRadius: '5px' }}>
+            <img src={selectedLocation.image} alt={selectedLocation.title} style={{ width: '200px', height: '150px', marginBottom: '10px' }} />
+            <h4>{selectedLocation.title}</h4>
+            <p>{selectedLocation.address}</p>
+          </div>
+        </MapInfoWindow>
+      )}
+      {hoveredLocation && (
+        <MapInfoWindow position={hoveredLocation.latlng} onClose={() => setHoveredLocation(null)}>
+          <div style={{ backgroundColor: 'white', padding: '10px', borderRadius: '5px' }}>
+            <img src={hoveredLocation.image} alt={hoveredLocation.title} style={{ width: '200px', height: '150px', marginBottom: '10px' }} />
+            <h4>{hoveredLocation.title}</h4>
+            <p>{hoveredLocation.address}</p>
+          </div>
+        </MapInfoWindow>
+      )}
+    </Map>
+  );
+};
 
-    <div className='view-map-wrapper'> 
-        <div className='view-map-box'>
-            
-        </div>
-    </div>
-
-
-
-
-
-
-    </>
-  )
-}
-
-export default ConnectTotalMap
+export default ConnectTotalMap;
