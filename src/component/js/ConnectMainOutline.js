54b041adca896c6c03d285792576a8f409fb0646
JeFeel <jaephil94@gmail.com>
2023-06-22 21:59:37 +0900

    [feature] ConnectMainOutline 게시판에 맞게 출력되도록 수정

--- src/component/js/ConnectMainOutline.js
+++ src/component/js/ConnectMainOutline.js
@@ -10,6 +10,7 @@ import ConnectLiveChatting from './ConnectLiveChatting';
 import '../scss/ConnectMainOutline.scss';
 import ConnectStoreSales from './ConnectStoreSales';
 
+
 const ConnectMainOutline = () => {
 
     const [isChangeOutline, setIsChangeOutline] = useState(true);
@@ -18,49 +19,51 @@ const ConnectMainOutline = () => {
 
     const closeChangeMenu = e => {
         const $menuClose = document.getElementById('CmoMenu');
-        
-        if($menuClose) {
-            $menuClose.style.animation = 'closeOutline 2s forwards 1';
-        }
-        
+        $menuClose.style.animation = 'closeOutline 2s forwards 1';
+
+        // {$location.pathname.startsWith('/contents/cvs/') ? (
+        //     <ConnectStoreSales />
+        // ) : ( ulLinkTag()
+        // )}
+
+
         setTimeout(() => {
-            setIsChangeOutline(false);
-            setIsChangeStore(false);
+            // setIsChangeOutline(false);
+            // setIsChangeStore(false);
         }, 1000);
     };
-    
+
     const openChangeMenu = e => {
         setIsChangeOutline(true);
     };
-    
-    
+
+
     const closeCreatePost = e => {
         setIsClosePost(false);
     };
-    
-    
+
+
     const [isClickColorHotPlace, setIsClickColorHotPlace] = useState(false);
     const [isClickColorFreeBoard, setIsClickColorFreeBoard] = useState(false);
     const [isClickColorPlaylist, setIsClickColorPlaylist] = useState(false);
-    
+
     const clickChangeColorHandlerHotPlace = () => {
         setIsClickColorHotPlace(!isClickColorHotPlace);
     };
-    
+
     const clickChangeColorHandlerFreeBoard = () => {
         setIsClickColorFreeBoard(!isClickColorFreeBoard);
     };
-    
+
     const clickChangeColorHandlerPlaylist = () => {
         setIsClickColorPlaylist(!isClickColorPlaylist);
     };
-    
-    
+
+
     const [activeButton, setActiveButton] = useState(null);
     const clickChangeColorHandler = (buttonId) => {
         setActiveButton(buttonId);
     };
-
 
     const $location = useLocation();
 
@@ -68,118 +71,136 @@ const ConnectMainOutline = () => {
         return null;
     }
 
-  return (
-    <>
 
-        <div className='connect-main-outline-wrapper'>
-            <header className='cmo-h1-box'>
-                <h1>우리동네 100% 즐기기!</h1>
-            </header>
-
-            <div className='cmo-container'>
-                {!isChangeStore && (
-                    <div
-                        className={`store-menu-modal-wrapper ${
-                            isChangeOutline ? 'closeOutline' : 'closeAnimation'
-                        }`}
-                        id='StoreModal'
-                    ><ConnectStoreSales />
-                </div>
+    const ulLinkTag  = e => {
+        return (
+        <>
+            <Link to={'/contents/hot-place'} className='cmo-list' id='Hot-Place'>
+                {isChangeOutline && (
+                    <div className='link-box'>
+                        <div className='list-box'>
+                            <button
+                                className={`cmo-menu-list hot-place list ${
+                                    activeButton === 'Hot-Place' ? 'normal-border' : 'clicked-border'
+                                }`}
+                                onClick={() => clickChangeColorHandler('Hot-Place')}
+                            ></button>
+                            <p className='cmo-text'>Hot Place</p>
+                        </div>
+                    </div>
                 )}
-                <ul className='cmo-menu' id='CmoMenu'>
-                    <Link to={'/contents/hot-place'} className='cmo-list' id='Hot-Place'>
-                        {isChangeOutline && (
-                            <div className='link-box'>
-                                <div className='list-box'>
-                                    <button
-                                        className={`cmo-menu-list hot-place list ${
-                                        activeButton === 'Hot-Place' ? 'normal-border' : 'clicked-border'
-                                        }`}
-                                        onClick={() => clickChangeColorHandler('Hot-Place')}
-                                    ></button>
-                                    <p className='cmo-text'>Hot Place</p>
-                                </div>
-                            </div>
-                        )}
-                    </Link>
-                    <Link to={'/contents/free-board'} className='cmo-list' id='Real-time-Chatting'>
-                        {isChangeOutline && (
-                            <div className='link-box'>
-                                <div className='list-box'>
-                                    <button className={`cmo-menu-list real-time-chatting list ${
-                                        activeButton === 'Real-time-Chatting' ? 'normal-border' : 'clicked-border'
-                                        }`}
-                                        onClick={() => clickChangeColorHandler('Real-time-Chatting')}
-                                        ></button>
-                                    <p className='cmo-text'>동네 자유 게시판</p>
-                                </div>
-                            </div>
-                        )}
-                    </Link>
-                    <Link to={'/nb-playlist'} className='cmo-list' id='Making-Friends'>
-                        {isChangeOutline && (
-                            <div className='link-box'>
-                                <div className='list-box'>
-                                    <button className={`cmo-menu-list making-friends list ${
-                                        activeButton === 'Making-Friends' ? 'normal-border' : 'clicked-border'
-                                        }`}
-                                        onClick={() => clickChangeColorHandler('Making-Friends')}
-                                        ></button>
-                                    <p className='cmo-text'>동네 플레이 리스트</p>
-                                </div>
-                            </div>
-                        )}
-                    </Link>
-                    <Link to={'/contents/cvs/GS25'} className='cmo-list' id='Closing-sale'>
-                        <button onClick={ closeChangeMenu } className='cmo-change-btn'>
-                            {isChangeOutline && (
-                                <div className='link-box'>
-                                    <div className='list-box'>
-                                        <button className={`cmo-menu-list closing-sale list ${
-                                        activeButton === 'Closing-sale' ? 'normal-border' : 'clicked-border'
-                                        }`}
+            </Link>
+            <Link to={'/contents/free-board'} className='cmo-list' id='Real-time-Chatting'>
+                {isChangeOutline && (
+                    <div className='link-box'>
+                        <div className='list-box'>
+                            <button className={`cmo-menu-list real-time-chatting list ${
+                                activeButton === 'Real-time-Chatting' ? 'normal-border' : 'clicked-border'
+                            }`}
+                                    onClick={() => clickChangeColorHandler('Real-time-Chatting')}
+                            ></button>
+                            <p className='cmo-text'>동네 자유 게시판</p>
+                        </div>
+                    </div>
+                )}
+            </Link>
+            <Link to={'/nb-playlist'} className='cmo-list' id='Making-Friends'>
+                {isChangeOutline && (
+                    <div className='link-box'>
+                        <div className='list-box'>
+                            <button className={`cmo-menu-list making-friends list ${
+                                activeButton === 'Making-Friends' ? 'normal-border' : 'clicked-border'
+                            }`}
+                                    onClick={() => clickChangeColorHandler('Making-Friends')}
+                            ></button>
+                            <p className='cmo-text'>동네 플레이 리스트</p>
+                        </div>
+                    </div>
+                )}
+            </Link>
+            <Link to={'/contents/cvs/GS25'} className='cmo-list' id='Closing-sale' >
+                {/*<button onClick={ closeChangeMenu } className='cmo-change-btn'>*/}
+                    {isChangeOutline && (
+                        <div className='link-box'>
+                            <div className='list-box'>
+                                <button className={`cmo-menu-list closing-sale list ${
+                                    activeButton === 'Closing-sale' ? 'normal-border' : 'clicked-border'
+                                }`}
                                         onClick={() => clickChangeColorHandler('Closing-sale')}
-                                        ></button>
-                                        <p className='cmo-text'>동네 편의점 정보</p>
-                                    </div>
-                                </div>
-                            )}
-                        </button>
-                    </Link>
-                </ul>
+                                ></button>
+                                <p className='cmo-text'>동네 편의점 정보</p>
+                            </div>
+                        </div>
+                    )}
+                {/*</button>*/}
+            </Link>
+        </>
+        )
+    };
 
-                {/* 고정 메인 메뉴 전환 게시판 box */}
-                <div className='cmo-change-board-box'>
-                    <div className='cmo-change-board'>
-                        <Routes>
-                            <Route path='/contents/hot-place' element={ <ConnectHotPlace closeCreatePost={closeCreatePost}/> }></Route>
-                        </Routes>
-                        <Routes>
-                            <Route path='/contents/free-board' element={ <ConnectFreeBoard closeCreatePost={closeCreatePost}/> }></Route>
-                        </Routes>
-                        <Routes>
-                            <Route path='/nb-live-chatting' element={ <ConnectLiveChatting /> }></Route>
-                        </Routes>
-                        <Routes>
-                            <Route path='/nb-playlist' element={ <ConnectPlayList /> }></Route>
-                        </Routes>
-                        <Routes>
-                            <Route path='/contents/cvs' element={ <ConnectStoreInfo /> }></Route>
-                        </Routes>
-                        <Routes>
-                            <Route path='/contents/cvs/:cvsname' element={ <ConnectStoreInfo /> }></Route>
-                        </Routes>
-                        <Routes>
-                            <Route path='/nb-closing-sale' element={<ConnectStoreSales />} />
-                        </Routes>
+    return (
+        <>
+
+            <div className='connect-main-outline-wrapper'>
+                <header className='cmo-h1-box'>
+                    <h1>우리동네 100% 즐기기!</h1>
+                </header>
+
+                <div className='cmo-container'>
+                    {/*{!isChangeStore && (*/}
+                    {/*    <div*/}
+                    {/*        className={`store-menu-modal-wrapper ${*/}
+                    {/*            isChangeOutline ? 'closeOutline' : 'closeAnimation'*/}
+                    {/*        }`}*/}
+                    {/*        id='StoreModal'*/}
+                    {/*    >*/}
+                    {/*        {$location.pathname === '/contents/cvs/*' ? (*/}
+                    {/*        <ConnectStoreSales />*/}
+                    {/*    ) : null}*/}
+                    {/*    </div>*/}
+                    {/*)}*/}
+                    {/*${ isChangeOutline ? 'closeOutline' : 'closeAnimation'}*/}
+
+                    <ul className={`cmo-menu`} id='CmoMenu' >
+                        {$location.pathname.startsWith('/contents/cvs/') ? (
+                                <ConnectStoreSales />
+                            ) : ( ulLinkTag()
+                    )}
+                    </ul>
+
+                    {/* 고정 메인 메뉴 전환 게시판 box */}
+                    <div className='cmo-change-board-box'>
+                        <div className='cmo-change-board'>
+                            <Routes>
+                                <Route path='/contents/hot-place' element={ <ConnectHotPlace closeCreatePost={closeCreatePost}/> }></Route>
+                            </Routes>
+                            <Routes>
+                                <Route path='/contents/free-board' element={ <ConnectFreeBoard closeCreatePost={closeCreatePost}/> }></Route>
+                            </Routes>
+                            <Routes>
+                                <Route path='/nb-live-chatting' element={ <ConnectLiveChatting closeCreatePost={closeCreatePost}/> }></Route>
+                            </Routes>
+                            <Routes>
+                                <Route path='/nb-playlist' element={
+                                    <ConnectPlayList />
+                                }></Route>
+                            </Routes>
+                            <Routes>
+                                <Route path='/contents/cvs' element={ <ConnectStoreInfo /> }></Route>
+                            </Routes>
+                            <Routes>
+                                <Route path='/contents/cvs/:cvsname' element={ <ConnectStoreInfo /> }></Route>
+                            </Routes>
+                            <Routes>
+                                <Route path='/nb-closing-sale' element={<ConnectStoreSales />} />
+                            </Routes>
+                        </div>
                     </div>
                 </div>
-
             </div>
-        </div>
-    </>
+        </>
 
-  )
+    )
 }
 
 export default ConnectMainOutline
