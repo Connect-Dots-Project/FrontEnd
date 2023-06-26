5c9600bcc5fa4f420a13e044a6f177adf0417033
sumin <330sum@naver.com>
2023-06-27 01:35:53 +0900

    핫플레이스 글쓰기 및 수정 기능 수정

--- src/component/js/ConnectCreatePost.js
+++ src/component/js/ConnectCreatePost.js
@@ -70,35 +70,32 @@ const ConnectCreatePost = ({ closeCreatePost, selectedHotplace, isEditMode }) =>
     }, 1000);
   };
 
-
-
-
   useEffect(() => {
     if (isEditMode && selectedHotplace) {
-
-      // console.log('------------------useEffect--------------------');
-  //     setHotplaceImg(selectedHotplace.hotplaceImg);
-  //     setHotplaceContent(selectedHotplace.hotplaceContent);
-  //     setHotplaceLatitude(selectedHotplace.hotplaceLatitude);
-  //     setHotplaceLongitude(selectedHotplace.hotplaceLongitude);
-  //     setHotplaceName(selectedHotplace.hotplaceName);
-  //     setHotplaceFullAddress(selectedHotplace.hotplaceFullAddress);
-  //     setKakaoLocation(selectedHotplace.kakaoLocation);
       setSelectedLocation(selectedHotplace.location);
-      
     }
   });
 
-  
-  
   const submitHandler = (e) => {
-  
     e.preventDefault();
-    
-    
+
+  
+   if ()
+
+    if (!selectedLocation) {alert ('행정구역을 선택해주세요.'); return;}
+    if (!hotplaceImg) {alert ('핫플레이스의 사진을 공유해주세요.'); return;}
+    if (!hotplaceContent) {alert ('핫플레이스의 글을 공유해주세요.'); return;}
+    if (!kakaoLocation) {alert ('지도에서 장소를 선택해주세요.'); return;}
+  
+    if (selectedLocation !== kakaoLocation) {
+      alert('행정구역과 지도의 장소가 일치하지 않습니다.');
+      return;
+    }
+
+
     const requestData = {
       location: selectedLocation,
-      hotplaceContent ,
+      hotplaceContent,
       memberIdx: 1,
       hotplaceLatitude,
       hotplaceLongitude,
@@ -106,38 +103,46 @@ const ConnectCreatePost = ({ closeCreatePost, selectedHotplace, isEditMode }) =>
       hotplaceFullAddress,
       kakaoLocation,
     };
-    
-    if (isEditMode) requestData.hotplaceIdx = selectedHotplace.hotplaceIdx;
-  
-  const jsonString = JSON.stringify(requestData);
-  const jsonDataBlob = new Blob([jsonString], { type: 'application/json' });
 
-  const hotplaceFormData = new FormData();
-  hotplaceFormData.append('hotplace', jsonDataBlob);
-  hotplaceFormData.append('hotplaceImg', hotplaceImg);
+    if (isEditMode) requestData.hotplaceIdx = selectedHotplace.hotplaceIdx;
+
+    const jsonString = JSON.stringify(requestData);
+    const jsonDataBlob = new Blob([jsonString], { type: 'application/json' });
+
+    const hotplaceFormData = new FormData();
+    hotplaceFormData.append('hotplace', jsonDataBlob);
+    hotplaceFormData.append('hotplaceImg', hotplaceImg);
 
     if (isEditMode) {
       fetch(REQUEST_URL, {
         method: 'PATCH',
         headers: {
-          'Authorization' : MyToken
+          'Authorization': MyToken
         },
         credentials: 'include',
         body: hotplaceFormData,
       })
         .then((res) => res.json())
         .then((result) => console.log(result));
+
+        
     } else {
       fetch(REQUEST_URL, {
         method: 'POST',
         headers: {
-          'Authorization' : MyToken
+          'Authorization': MyToken
         },
         credentials: 'include',
         body: hotplaceFormData,
       })
         .then((res) => res.json())
-        .then((result) => console.log(result.isWrite));
+        .then((result) => {
+          if (result.isWrite) {
+            alert('저장되었습니다.');
+          } else {
+            alert('저장에 실패했습니다.');
+          }
+        })
     }
 
     window.location.reload();
@@ -167,16 +172,10 @@ const ConnectCreatePost = ({ closeCreatePost, selectedHotplace, isEditMode }) =>
 
               <div className='cp-main-box'>
                 <div className='cp-main'>
-                  
-                    <ConnectWriteBoard
-                      // wbHotplaceImg={selectedHotplace.hotplaceImg}
-                      // wbHotplaceContent={selectedHotplace.hotplaceContent}
-              
-                      setHotplaceImg = {setHotplaceImg}
-                      setHotplaceContent = {setHotplaceContent}
-               
-                    />
-                
+                  <ConnectWriteBoard
+                    setHotplaceImg={setHotplaceImg}
+                    setHotplaceContent={setHotplaceContent}
+                  />
                 </div>
               </div>
 
