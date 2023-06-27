6002ab6befd65bda2d6b1ba3653127dad6213275
JeFeel <jaephil94@gmail.com>
2023-06-27 20:58:59 +0900

    [feature] ConnectLogin 주석 삭제

--- src/component/js/ConnectLogin.js
+++ src/component/js/ConnectLogin.js
@@ -422,9 +422,7 @@ const ConnectLogin = () => {
         // 1. 로컬 스토리지 - 브라우저가 종료되어도 보관 (자동 로그인)
         // 2. 세션 스토리지 - 브라우저가 종료되면 사라짐 (자동 로그아웃)
         localStorage.setItem('isLogInTest', 'true');
-        // localStorage.setItem('ACCESS_TOKEN', token);
-        // localStorage.setItem('LOGIN_USERNAME', 'test1');
-        // localStorage.setItem('USER_ROLE', 'role');
+
 
         // 홈으로 리다이렉트
         // redirection('/');
