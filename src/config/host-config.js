3cadfdf90e6296ca457cf9f70064f535a375968f
sumin <330sum@naver.com>
2023-06-28 14:40:59 +0900

    핫플레이스 수정

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -8,7 +8,7 @@ let backEndHostName; // 백엔드 서버 호스트 이름
 // if (clientHostName === 'localhost') {
   // backEndHostName = 'http://localhost:8181';
 // } else if (clientHostName === '330-special-bucket.s3-website.ap-northeast-2.amazonaws.com') {
-  backEndHostName = 'http://13.125.104.141';
+  backEndHostName = 'http://connect-dots.site';
 // }
 
 export const API_BASE_URL = backEndHostName;
