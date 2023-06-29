b7689393b180c17e9a3f7075f3674966470fb63e
ejdrp1 <ejdrp1@naver.com>
2023-06-29 21:13:59 +0900

    06.29 마이페이지 구현 완료

--- src/component/js/ConnectUserActivityFreeBoardReply.js
+++ src/component/js/ConnectUserActivityFreeBoardReply.js
@@ -1,7 +1,7 @@
 // import React from "react";
 import React, { useEffect } from 'react';
 import { useState } from 'react';
-import '../scss/ConnectUserActivityFreeBoard.scss';
+import '../scss/ConnectUserActivityFreeBoardReply.scss';
 import { getLoginUserInfo } from '../../util/login-util';
 import { API_BASE_URL } from "../../config/host-config";
 
@@ -33,36 +33,52 @@ const ConnectUserActivityFreeBoard = () => {
     return (
         <>
         
-        <div id="UserActivityFreeBoardWrapper">
-            <div className="user-activity-free-board">
+        <div id="UserActivityFreeBoardReplyWrappers">
+            <div className="user-activity-free-boards">
 
-                <div className="user-info-list-box">
-                {freeboardItems.map((item, index) => (
-                    <div className="user-info-list" key={index}>
-                        <div className="ui-category">
-                            <p>{item.category}</p>
+                <div className='user-info-list-wrappers'>
+
+                <div className="user-info-list-boxes">
+
+                    {freeboardItems.map((item, index) => (
+
+                        <div className='user-info-lists-boxes'>
+
+
+                        <div className="user-info-lists" key={index}>
+                            <div className='ui-list-boxes'>
+                                <div className="ui-categories">
+                                    <p>{item.category}</p>
+                                </div>
+                                <div className="ui-locations">
+                                    <p>{item.location}</p>
+                                </div>
+                                <div className="ui-titles">
+                                    <p>{item.freeBoardTitle}</p>
+                                </div>
+                                <div className="ui-dates">
+                                    <p>{item.freeBoardWriteTime}</p>
+                                </div>
+                            </div>
+
+
+                            <div className="ui-guest-reply-boxes">
+                                <div className='ui-guest-replies'>
+                                    <p>{item.content}</p>
+                                </div>
+                            </div>
+                            
                         </div>
-                        <div className="ui-location">
-                            <p>{item.location}</p>
-                        </div>
-                        <div className="ui-title">
-                            <p>{item.freeBoardTitle}</p>
-                        </div>
-                        <div className="ui-date">
-                            <p>{item.freeBoardWriteTime}</p>
-                        </div>
-                        <div className="ui-date">
-                            <p>여기가 댓글 내용임 피그마 참조해서 수정{item.content}</p>
-                        </div>
+
                     </div>
-                        ))}
+                ))}
                    
-
                 </div>
 
 
 
 
+                    </div>
             </div>
         </div>
 
